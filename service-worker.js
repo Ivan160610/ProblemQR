a285daaea185b06b4a0cd19deb96399bf652179e
Наставники Склад <NastavnikiSklad@office.garsalko.ru>
2026-06-14 16:14:19 +0700

    Обновление сайта

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,51 @@
-const CACHE = "promax-v1";
+const CACHE = 'promax-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './JS/xlsx.full.min.js'
+];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./js/xlsx.full.min.js"
-      ]);
-    })
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
   );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.open(CACHE).then(cache =>
+      cache.match(event.request).then(cachedResponse => {
+        const networkFetch = fetch(event.request).then(networkResponse => {
+          if (networkResponse && networkResponse.status === 200) {
+            cache.put(event.request, networkResponse.clone());
+          }
+          return networkResponse;
+        }).catch(() => cachedResponse || caches.match('./index.html'));
+
+        return cachedResponse || networkFetch;
+      })
+    )
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
