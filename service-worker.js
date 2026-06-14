7774565cbf0c8f4d9d0db73bf6ea7917c93771c6
Наставники Склад <NastavnikiSklad@office.garsalko.ru>
2026-06-14 15:45:36 +0700

    Подключил VS Code

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE = "promax-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./js/xlsx.full.min.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
